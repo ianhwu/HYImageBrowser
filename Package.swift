85e32f21946d16abaf629376da4d2d76b1d405da
huyan_18040019 <huyan@shinhofood.com>
2019-10-22 17:49:07 +0800

    add kingfisher

--- Package.swift
+++ Package.swift
@@ -14,16 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(path: "https://github.com/onevcat/Kingfisher.git")
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HYImageBrowser",
-            dependencies: []),
-        .testTarget(
-            name: "HYImageBrowserTests",
-            dependencies: ["HYImageBrowser"]),
+            dependencies: ["Kingfisher"],
+            path: "Sources")
     ]
 )
